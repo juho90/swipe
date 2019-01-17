723aa09b81daed85f3fb90c959bb30b380607a99
Juho Han <hanjuho1218@gmail.com>
2019-01-17 15:42:44 +0900

    edit code

--- src/game/swipe/swipebrickbreakerapp.ts
+++ src/game/swipe/swipebrickbreakerapp.ts
@@ -7,22 +7,14 @@ export default class SwipeBrickBreakerApp {
     public endGame: boolean;
     public dtime: number;
 
-    constructor() {
-        this.onEnterState = this.onEnterState.bind(this);
-        this.ready = this.ready.bind(this);
-        this.run = this.run.bind(this);
-        this.end = this.end.bind(this);
-        this.next = this.next.bind(this);
-    }
-
     public init(w: number, h: number, cell: number): void {
         this.swipe = new SwipeBrickBreaker(w, h, cell);
         this.fsm = new FiniteStateMachine;
-        this.fsm.onEnterState = this.onEnterState;
-        this.fsm.register("ready", this.ready);
-        this.fsm.register("run", this.run);
-        this.fsm.register("end", this.end);
-        this.fsm.register("next", this.next);
+        this.fsm.onEnterState = this.onEnterState.bind(this);
+        this.fsm.register("ready", this.ready.bind(this));
+        this.fsm.register("run", this.run.bind(this));
+        this.fsm.register("end", this.end.bind(this));
+        this.fsm.register("next", this.next.bind(this));
         this.endGame = false;
         this.dtime = 0;
         this.fsm.set("ready");
